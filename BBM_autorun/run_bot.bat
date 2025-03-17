96ee558d64ebe5ced53e9b083f4356446d664716
♰Burning_Head♰ <nikitakutergin59@gmail.com>
2025-03-17 16:55:44 +0300

    Delete BBM_autorun directory

--- BBM_autorun/run_bot.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd C:\Users\user\.vscode\calculator
-set PATH=%PATH%;C:\Program Files\Go\bin
-set BOT_TOKEN=8183158892:AAE55IpXKtzipOyTq7TeCAMCkCE5j3mfVTA
-"C:\Program Files\Go\bin\go" run bot.go
-pause
